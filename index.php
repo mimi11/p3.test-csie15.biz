218568c2fae556d56872cf426e45e0c4302cfa97
Carine <cmelhorn@g.harvard.edu>
2013-11-16 16:42:21 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Rent-a-Room</title>
+
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/features.css" type="text/css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+</head>
+
+
+<body>
+
+<div id='wrapper'>
+
+    <h1>Rent-A-Room</h1>
+
+    <!-- Left side with all the controls -->
+    <div id='controls'>
+
+        <!-- Ad Background color picker -->
+        <h2>Pick Background Color </h2>
+
+
+        <!-- Texture picture -->
+        <h2>Select Available room </h2>
+
+        <!--Room Amneties -->
+        <h2>Amenities:</h2>
+        <!-- Message -->
+        <h2>Room Ad message here</h2>
+
+
+        <!-- Move in Requirements -->
+        <h2>Move-in Requirements</h2>
+
+
+
+
+
+    </div>
+
+    <!-- Right side with the live preview -->
+    <div id='preview'>
+
+         <div id=house-background>
+             <div id='room-view'> </div>
+             <div id ='amenities'></div>
+         </div>
+         <div id='ad-message'>
+             <div id='rentValues'></div>
+             <div id='rentRequirements'></div>
+             <div id='rentMessage'></div>
+         </div>
+
+        <!-- Buttons -->
+        <input type='button' id='refresh-btn' value='Start over'>
+        <input type='button' id='print-btn' value='Print'>
+    </div>
+
+</div>
+
+<script src="js/rent_a_room.js"></script>
+
+</body>
+
+</html>
---
+++ js/rent_a_room.js
@@ -0,0 +1,19 @@
+/*
+Rent-a-Room application is a quick an easy way for Landlord or roomate to post a new Ad using external services such as Craiglist
+and refer them to the one-pager view that is filled out in just few clicks.
+
+ */
+
+//1. Request user to Upload room Pictures in gallery
+
+
+
+//2. Select Available room pictures from gallery
+
+$('room').click(function(){
+    var room_that_was_clicked=$(this).css('background-image');
+    $('#canvas').css('background-image', texture_that_was_clicked);
+
+
+
+});
